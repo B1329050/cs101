c97b2cb08da3a325daf2326f66fee054e5e33f53
B1329050 <weiwei.0522@gmail.com>
2024-12-05 15:27:48 +0800

    Create main4.c

---
+++ EX3/main4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int i = 2100;
+    if (i%roo==0||(i%4==0&&i%100 !=0)){
+        peintf("閏年");
+    }
+    else{
+        printf("不是閏年");
+    }
+    return 0;
+}
