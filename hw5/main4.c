27d67a26660377280899385781d098ac9c862878
B1329050 <weiwei.0522@gmail.com>
2024-12-11 22:59:30 +0800

    Create main4.c

---
+++ hw5/main4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int i = 12345;
+    int a = i % 10;
+    int b = (i/1000)%10;
+    int k = i - a - b *1000 + b + a*1000;
+    printf ("%d",k);
+    return 0;
+}
