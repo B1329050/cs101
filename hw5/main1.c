25903c530a0b07b648abf9f5de82e13d6e90a2d2
B1329050 <weiwei.0522@gmail.com>
2024-12-11 20:47:07 +0800

    Create main1.c

---
+++ hw5/main1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n = 7; 
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n - i; j++) {
+            printf(" "); 
+        }
+        for (j = 1; j <= i; j++) {
+            printf("%d ", i);
+        }
+        printf("\n"); // 換行
+    }
+
+    return 0;
+}
