479e91b9867def0df1d7d900918d66ffa19fee81
B1329050 <weiwei.0522@gmail.com>
2024-12-12 15:07:51 +0800

    Create main1.c

---
+++ ex4/main1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows = 7;
+    int i, j ;
+    
+    for (i =1; i <= rows; i++){
+        for(j = 1; j <= rows-i;j++){
+        printf(" ");
+        }
+        for (j=1; j <= i; j++){
+        printf("%d ", i);
+    }
+    printf("\n");
+ }
+return 0;
+}
